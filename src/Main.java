25a785e47fe649916922e475829e62dcdeb89c4a
Tu Nombre <tonitf@gmail.com>
2025-03-24 20:18:12 +0100

    inicio_java 2

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,29 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int menuseleccionado;
+        String menu= """
+                1. Nuevo alumno
+                2. Listar alumnos
+                ...
+                10. Salir
+                """;
+        do{
+            System.out.println(menu);
+            menuseleccionado=scanner.nextInt();
+            switch (menuseleccionado){
+                case 1:
+                    System.out.println("Nuevo alumno");
+                    break;
+                case 2:
+                    System.out.println("Listar alumnos");
+                    break;
+                default:
+                    System.out.println("Opción no válida");
+            }
+        }while(scanner.nextInt()!=10);
+
+
         ArrayList<Integer> numeros = new ArrayList<>();
         ArrayList<Integer> pares = new ArrayList<>();
         ArrayList<Integer> impares = new ArrayList<>();
